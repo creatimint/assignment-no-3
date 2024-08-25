a05a60728344d742d8d0d5a7fbc54e58da078c1d
Foysal <creatimint@gmail.com>
2024-08-25 20:04:10 +0600

    footer section added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-      extend: {},
+      extend: {
+        backgroundImage: {
+          'texture': "url('./assets/bg.jpg')",
+        }
+      },
     },
     plugins: [],
   }
